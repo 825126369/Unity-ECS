2b855e17440005ab51c7bd324e21e1fbae195261
xuke <825126369@qq.com>
2025-09-06 10:50:31 +0800

    2

--- Assets/ECSExample/生命周期/MoveSystem.cs
+++ Assets/ECSExample/生命周期/MoveSystem.cs
@@ -22,7 +22,7 @@ public partial class MoveSystem : SystemBase
                 moveSpeed.x = 4;
             }
 
-            Debug.Log("移动： " + speed);
+           // Debug.Log("移动： " + speed); //使用Debug 会导致 报错：编辑器内存泄露错误
             transform.Position += new float3(moveSpeed.x, 0, 0) * deltaTime; // 每秒向右移动 1 单位
 
             // 或者直接设置一个新位置
